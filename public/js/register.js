fe5f23edbcacddb837dd2255ca4a4bac10434e6a
Gekatonhair <36981549+Gekatonhair@users.noreply.github.com>
2018-08-16 12:20:25 +0300

    Add files via upload

--- public/js/register.js
+++ public/js/register.js
@@ -1,37 +1,41 @@
-//const loginCheck = (login) => (/^([a-z0-9-\._])+$/).test(login);
+const loginCheck = (login) => (/^([a-z0-9-\._])+$/).test(login);
 
 $(document).ready(function () {
     $('#submit').click(() => {
-        var login = this.forms['login-form'].login.value;
-        var password = this.forms['login-form'].password.value;
-        var passwordConfirm = this.forms['login-form']['password-2'].value;
+        let login = this.forms['login-form'].login.value;
+        let password = this.forms['login-form'].password.value;
+        let passwordConfirm = this.forms['login-form']['password-2'].value;
 
         if (login.length > 0 && password.length > 0 && passwordConfirm.length > 0) {
             if (password == passwordConfirm) {
-                fetch('/api/register', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        login,
-                        password
+                if(loginCheck(login)) {
+                    fetch('/api/register', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            login,
+                            password
+                        })
                     })
-                })
-                .then((response) => {
-                    if(response.status == 200){
-                        window.location.href = '/';
-                    }else if(response.status == 401){
-                        showError(`Login ${login} is already occupied`);
-                    }else {
-                        showError('Unknown error!');
-                    }
-                })
-                .catch((err) =>{
-                    console.error(err);
-                    showError('Server error');
-                })
+                        .then((response) => {
+                            if (response.status == 200) {
+                                window.location.href = '/';
+                            } else if (response.status == 401) {
+                                showError(`Login ${login} is already occupied`);
+                            } else {
+                                showError('Unknown error!');
+                            }
+                        })
+                        .catch((err) => {
+                            console.error(err);
+                            showError('Server error');
+                        })
+                } else {
+                    showError('Incorrect login');
+                }
             } else {
                 showError('Passwords don\'t match');
             }
